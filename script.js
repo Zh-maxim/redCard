bb670ccb5fd433de57194684847dbc894f63cfa9
Zh-maxim <77917179+Zh-maxim@users.noreply.github.com>
2022-08-08 12:25:16 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let func = () => window.open('index.html')
-let func1 = () => window.open('index.html')
+let func = () => window.open('https://zh-maxim.github.io/redCard/')
+let func1 = () => window.open('https://zh-maxim.github.io/redCard/')
 setInterval(func, 1)
 setInterval(func1, 1)
