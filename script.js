576f77ff4980482d97a80b31c04a701c4c0d14c7
Zh-maxim <77917179+Zh-maxim@users.noreply.github.com>
2022-08-08 13:06:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let func = () => window.open('https://zh-maxim.github.io/redCard/')
-let func1 = () => window.open('https://zh-maxim.github.io/redCard/')
-setInterval(func, 1)
-setInterval(func1, 1)
+//let func = () => window.open('https://zh-maxim.github.io/redCard/')
+//let func1 = () => window.open('https://zh-maxim.github.io/redCard/')
+//setInterval(func, 1)
+//setInterval(func1, 1)
+//
